aa3bbb040224d7f364e513e263ff49d246508069
OsmanDeniz <osmandeniz.ceng@gmail.com>
2021-04-22 01:56:27 +0300

    Geriye IDataResult donen tipler eklendi. Icerigi ise durum bilgisi,mesaj ve geriye donen veri olmaktadir.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,27 +24,27 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            return new DataResult( _productDal.GetAll());
+            return new DataResult<List<Product>>(true, _productDal.GetAll());
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p => p.ProductId == id);
+            return new DataResult<List<Product>>(true, _productDal.GetAll(p => p.ProductId == id));
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return new DataResult<List<Product>>(true, _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            return new DataResult<List<ProductDetailDto>>(true, _productDal.GetProductDetails());
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return _productDal.Get(p => p.ProductId == productId);
+            return new DataResult<Product>(true, _productDal.Get(p => p.ProductId == productId));
         }
 
         public IResult Add(Product product)
---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Core.Utilities.Results
+{
+    /// <summary>
+    /// Burada geriye bir turde veri doner ve mesajlar IResult uzerinden kullanilir.
+    /// Ornek : T degeri List<Product> olabilir. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDataResult<T> : IResult
+    {
+        T Data { get; }
+    }
+}
