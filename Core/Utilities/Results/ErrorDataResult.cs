eec0aebb6eaddc9bd69d62bb367c4110bf616a74
OsmanDeniz <osmandeniz.ceng@gmail.com>
2021-04-26 18:27:18 +0300

    Core katmanina Success ve Error Data Result eklendi

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, string msg) : base(false, msg, data)
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(false, data)
+        {
+
+        }
+
+        public ErrorDataResult(string msg) : base(false, msg, default)
+        {
+        }
+
+        public ErrorDataResult() : base(false, default)
+        {
+
+        }
+    }
+}
---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        public SuccessDataResult(T data, string msg) : base(true, msg, data)
+        {
+
+        }
+
+        public SuccessDataResult(T data) : base(true, data)
+        {
+
+        }
+
+        public SuccessDataResult(string msg) : base(true, msg, default)
+        {
+        }
+
+        public SuccessDataResult():base(true,default)
+        {
+            
+        }
+    }
+}
